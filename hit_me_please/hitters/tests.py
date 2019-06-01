cd4479068e46112bcd846e9f0980794846566840
Gatuk S Chattanon <gatukgl@gmail.com>
2019-06-01 12:14:16 +0700

    ✨ Show email in hitter admin :baby_chick:

--- hit_me_please/hitters/tests.py
+++ hit_me_please/hitters/tests.py
@@ -18,3 +18,8 @@ class HitterAdminTest(TestCase):
     def test_hitter_admin_should_be_registered_to_admin(self):
         actualHitterInstance = admin.site._registry[Hitter]
         self.assertIsInstance(actualHitterInstance, HitterAdmin)
+
+    def test_hitter_admin_should_display_email(self):
+        expected = ('email',)
+
+        self.assertEqual(HitterAdmin.list_display, expected)
