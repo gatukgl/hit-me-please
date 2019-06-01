a37903d1eaa8b024b86438236877d24ab00a3333
Gatuk S Chattanon <gatukgl@gmail.com>
2019-06-01 15:16:57 +0700

    ✨ Add header for form :baby_chick:

--- hit_me_please/hitters/tests/test_views.py
+++ hit_me_please/hitters/tests/test_views.py
@@ -7,11 +7,13 @@ class LandingPageViewTest(TestCase):
     def test_view_should_have_form_with_email_field_and_submit_button(self):
         response = self.client.get('/')
 
+        expected = '<h1>Hit Me!</h1>'
+        self.assertContains(response, expected, status_code=200)
         expected = '<form action="." method="post">'
         self.assertContains(response, expected, status_code=200)
         expected = '<input type="hidden" name="csrfmiddlewaretoken"'
         self.assertContains(response, expected, status_code=200)
-        expected = '<input type="email">'
+        expected = '<input type="email" name="email">'
         self.assertContains(response, expected, status_code=200)
         expected = '<button type="submit">Submit</button>'
         self.assertContains(response, expected, status_code=200)
