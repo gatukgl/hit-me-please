bde8be16d9614c466a48a80b6795f26f3bf8f48b
Gatuk S Chattanon <gatukgl@gmail.com>
2019-06-01 14:23:37 +0700

    ✨ Add landing page to enter email and submit :baby_chick:

--- hit_me_please/hitters/views.py
+++ hit_me_please/hitters/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        html = '<form action="." method="post">'
+        html += '<input type="email">'
+        html += '<input type="submit">Submit</input>'
+        return HttpResponse(html)
