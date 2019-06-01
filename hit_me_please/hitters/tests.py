bfa2f924d6b801666740a892fd679525911a869e
Gatuk S Chattanon <gatukgl@gmail.com>
2019-06-01 11:20:04 +0700

    🗃 Add hitter model and field :baby_chick:

--- hit_me_please/hitters/tests.py
+++ hit_me_please/hitters/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Hitter
+
+
+class HitterTest(TestCase):
+    def test_hitter_model_should_have_email_field(self):
+        hitter = Hitter.objects.create(
+            email='gatuk@prontomaketing.com'
+        )
+
+        self.assertEqual(hitter.email, 'gatuk@prontomaketing.com')
